158fb03157078849d51497f88c2555388f6d21c2
Tony Wang <Tony94042@gmail.com>
2023-11-29 17:31:15 +1100

    Started to implement logic for map

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import { User } from './User'
 import { Company } from "./company";
+import { CustomerMap } from './CustomerMaps';
 
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-    zoom: 1,
-    center: {
-        lat: 0,
-        lng: 0
-    }
-} )
+const user = new User();
+const company = new Company()
+const customMap = new CustomerMap('map');
+
+customMap.addUserMarker(user)
+customMap.addCompanyMarker(company)
